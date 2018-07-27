adeb68e35b4a01ce71b1539e6adbd54c3b673d88
artday <admin@artday.com.ua>
2018-07-27 06:09:09 +0300

    add bootstrap, jquery, popper to template

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -9,8 +9,7 @@ window._ = require('lodash');
 
 try {
     window.$ = window.jQuery = require('jquery');
-
-    require('bootstrap-sass');
+    window.Popper = require('popper');
 } catch (e) {}
 
 /**
