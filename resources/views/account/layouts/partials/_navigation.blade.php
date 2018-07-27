2bb5f14c58d85b97fe6dbd7c024175b9d69cd980
artday <admin@artday.com.ua>
2018-07-28 00:29:02 +0300

    templating

--- resources/views/account/layouts/partials/_navigation.blade.php
+++ resources/views/account/layouts/partials/_navigation.blade.php
@@ -1,11 +1,15 @@
-<ul class="nav nav-pills flex-column">
-    <li class="nav-item">
-        <a class="nav-link" href="{{ route('account.index') }}">Account overview</a>
-    </li>
-    <li class="nav-item">
-        <a class="nav-link" href="{{ route('account.profile.index') }}">Profile</a>
-    </li>
-    <li class="nav-item">
-        <a class="nav-link" href="{{ route('account.password.index') }}">Change password</a>
-    </li>
-</ul>
+<div class="card">
+    <div class="card-body">
+        <ul class="nav nav-pills flex-column">
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('account.index') }}">Account overview</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('account.profile.index') }}">Update profile</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('account.password.index') }}">Change password</a>
+            </li>
+        </ul>
+    </div>
+</div>
