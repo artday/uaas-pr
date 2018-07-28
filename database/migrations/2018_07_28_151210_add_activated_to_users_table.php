99e47e35f2ef90afaa09fcc925f3d0e8c65d5032
artday <admin@artday.com.ua>
2018-07-28 18:15:50 +0300

    add activated field to users table

--- database/migrations/2018_07_28_151210_add_activated_to_users_table.php
+++ database/migrations/2018_07_28_151210_add_activated_to_users_table.php
@@ -14,7 +14,7 @@ class AddActivatedToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->boolean('activated')->default(false);
         });
     }
 
@@ -26,7 +26,7 @@ class AddActivatedToUsersTable extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->drop('activated');
         });
     }
 }
