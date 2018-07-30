ba4e3abf0a97a8a2104fa9a08da0999c11328e3f
artday <admin@artday.com.ua>
2018-07-30 20:40:09 +0300

    protecting forgot password if user is not active

--- app/Http/Controllers/Auth/ForgotPasswordController.php
+++ app/Http/Controllers/Auth/ForgotPasswordController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\ForgotPasswordRequest;
 use Illuminate\Foundation\Auth\SendsPasswordResetEmails;
+use Illuminate\Support\Facades\Password;
 
 class ForgotPasswordController extends Controller
 {
@@ -29,4 +31,19 @@ class ForgotPasswordController extends Controller
     {
         $this->middleware('guest');
     }
+
+    /* Override trait to use custom form request */
+    public function sendResetLinkEmail(ForgotPasswordRequest $request)
+    {
+        $this->validateEmail($request);
+        $response = $this->broker()->sendResetLink(
+            $request->only('email')
+        );
+
+        return $response == Password::RESET_LINK_SENT
+            ? $this->sendResetLinkResponse($response)
+            : $this->sendResetLinkFailedResponse($request, $response);
+    }
+
+
 }
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use App\Models\ConfirmationToken;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Schema;
@@ -24,6 +25,12 @@ class AppServiceProvider extends ServiceProvider
         /* Check if password is current password */
         Validator::extend('is_current_password', function ($attribute, $value, $parameters, $validator) {
             return Hash::check($value, auth()->user()->password);
+        });
+
+        /* Validate is true table column. Example: 'email'=>'is_true:user,activated' */
+        Validator::extend('is_true', function ($attribute, $value, $parameters, $validator){
+            $user = DB::table($parameters[0])->where($attribute, $value)->where($parameters[1], true)->first();
+            return($user && $user->$attribute === $value);
         });
 
         Route::model('confirmation_token', ConfirmationToken::class);
